887aed0e941008f1c5e6a4b33cc1830012c247ff
Ronny Welter <ronny.welter@persgroep.be>
2016-06-26 01:15:17 +0200

    entirely rewrote the block selector approach; this looks REALLY solid. Now to bring back the block level approach

--- index.js
+++ index.js
@@ -1,137 +1,211 @@
 'use strict';
 var path = require('path'),
 	fs = require('fs'),
+	merge = require('merge'),
 	postcss = require('postcss'),
-	critical = {
-		'start': 'critical:start',
-		'end': 'critical:end'
-	},
+	userOptions = null,
 	criticalActive = false,
-	latestRule = null,
-	latestRuleAdded = false,
-	temp = null;
+	numberOfLinesInOriginalCss = 0,
+	numberOfSameChecks = 0,
+	numberOfSelectorSearches = 0,
+	defaults = {
+		'start': 'critical:start',
+		'end': 'critical:end',
+		'suffix': '-critical'
+	};
 
-function createCriticalFilename(filename, suffix) {
+function CriticalSplit(newOptions) {
+	newOptions = newOptions || {};
+
+	return function(originalCss, result) {
+		if (applyUserOptions(newOptions)) {
+			performTransform(originalCss, result);
+		};
+	};
+}
+
+function performTransform(originalCss, result) {
+	var criticalCss = postcss.root(),
+		absolutePath = originalCss.source.input.file,
+		cwd = process.cwd(),
+		relativePath = path.relative(cwd, absolutePath),
+		nonCriticalFilename = path.basename(relativePath),
+		relativeDirectory = path.dirname(relativePath),
+		criticalFilename = createCriticalFilename(nonCriticalFilename);
+
+	getAllCriticals(originalCss, criticalCss);
+
+	// console.log('numberOfLinesInOriginalCss:', numberOfLinesInOriginalCss);
+	// console.log('numberOfSameChecks:', numberOfSameChecks);
+	// console.log('numberOfSelectorSearches:', numberOfSelectorSearches);
+
+	fs.writeFileSync(path.join(relativeDirectory, nonCriticalFilename), originalCss.toResult());
+
+	if (criticalCss.nodes.length > 0) {
+		fs.writeFileSync(path.join(relativeDirectory, criticalFilename), criticalCss.toResult());
+	}
+}
+
+function applyUserOptions(newOptions) {
+	var errorMessage ='',
+		result = true;
+
+	userOptions = merge(defaults, newOptions);
+
+	if (userOptions.start === userOptions.end) {
+		errorMessage += '\n\n';
+		errorMessage += 'ERROR :: PostCSS Plugin: Critical Split\n';
+		errorMessage += '.Critical CSS start and end tag must not be the same. \n';
+		errorMessage += 'Please adapt your options. \n\n';
+		errorMessage += '------ Current Options ----- \n\n';
+		errorMessage += JSON.stringify(userOptions, null, 2) + '\n\n';
+		errorMessage += '---------- End -------------\n';
+
+		console.error(errorMessage);
+		result = false;
+	}
+
+	return result;
+}
+
+function createCriticalFilename(filename) {
 	var position = filename.lastIndexOf('.css'),
 		result = '';
 
 	result = filename.substring(0, position);
-	result += suffix;
+	result += userOptions.suffix;
 	result += '.css';
 
 	return result;
 }
 
-function clearRule(rule) {
-	rule.walk(function(nestedRule) {
+function clearLevel(level) {
+	level.walk(function(nestedRule) {
 		nestedRule.remove();
 	});
 }
 
-function CriticalSplit(options) {
-	var options = options || {},
-		filenameSuffix = '-critical',
-		filename = '';
+function getAllCriticals(originalCss, criticalCss) {
+	var currentLevel = null;
 
-	if (typeof options.suffix !== 'undefined') {
-		filenameSuffix = options.suffix;
-	}
+	originalCss.walk(function(line) {
+		var temp = null;
 
-	return function(originalCss, result) {
-		var criticalCss = postcss.root(),
-			absolutePath = originalCss.source.input.file,
-			cwd = process.cwd(),
-			relativePath = path.relative(cwd, absolutePath),
-			nonCriticalFilename = path.basename(relativePath),
-			relativeDirectory = path.dirname(relativePath),
-			criticalFilename = createCriticalFilename(nonCriticalFilename, filenameSuffix);
+		numberOfLinesInOriginalCss++;
 
-		originalCss.walk(processRule.bind(null, criticalCss)); // all rules
-
-		fs.writeFileSync(path.join(relativeDirectory, nonCriticalFilename), originalCss.toResult());
-
-		if(criticalCss.nodes.length > 0) {
-			fs.writeFileSync(path.join(relativeDirectory, criticalFilename), criticalCss.toResult());
+		if (line.type === 'comment' && line.text === userOptions.start) {
+			criticalActive = true;
+			// line.remove(); // remove unnecessary comment
+		} else if (line.type === 'comment' && line.text === userOptions.end) {
+			criticalActive = false
+			currentLevel = null;
+			// line.remove(); // remove unnecessary comment
+		} else if (criticalActive === true && (line.type === 'decl' || line.type === 'comment')) {
+			appendDeclaration(criticalCss, line);
+			line.remove();
 		}
-	};
+	});
 }
 
-function processRule(parentRule, rule) {
-	var newRule = null,
-		allowInherit = true;
+function appendDeclaration(criticalCss, line) {
+	var parents = getParents(line),
+		currentLevel = prepareSelectors(criticalCss, parents);
 
-	if (rule.type === 'comment' && rule.text === critical.start) {
-		criticalActive = true;
-		latestRuleAdded = false;
+	currentLevel.append(line);
+	currentLevel.raws.semicolon = true; // enforce last rule semicolon; just in case of some weird last-line comments...
+}
 
-		if (rule.parent.type === 'rule') {
-			latestRule = rule.parent.clone();
-			clearRule(latestRule);
-			//console.log('newline', latestRule.parent);
-		}
+function prepareSelectors(criticalCss, selectorLevels) {
+	var currentLevel = null;
 
-		rule.remove();
-	} else if (rule.type === 'comment' && rule.text === critical.end) {
-		criticalActive = false;
-		latestRule = null;
-		rule.remove();
-	} else if(criticalActive === true) {
-		switch (rule.type) {
-			case 'atrule':
-			case 'rule':
-				if (latestRuleAdded === true && latestRule !== null) {
-					parentRule = latestRule;
-					console.log('captured the latestRule');
-				}
+	currentLevel = findSelector(criticalCss, selectorLevels);
 
-				newRule = rule.clone();
-				parentRule.append(newRule);
-
-				clearRule(newRule);
-
-				//to understand recursion you first need to understand recursion
-				rule.walk(processRule.bind(null, newRule));
-
-				break;
-			case 'comment':
-			case 'decl':
-				if (latestRule !== null) {
-					// console.log('rule.parent:', rule.parent.selector);
-					// console.log('inherited parent:', parentRule.selector);
-					// console.log('latest:', latestRule.selector);
-
-					if (typeof parentRule.selector !== 'undefined' && parentRule.selector === rule.parent.selector){
-						console.log('!! !! !! mismatch detected');
-						allowInherit = false;
-					} else {
-						allowInherit = true;
-					}
-
-					console.log('------------------');
-					//latestRule = null;
-				}
-
-				if (allowInherit === true && latestRule !== null) {
-					if (latestRuleAdded === false) {
-						//console.log('need parent rule');
-						parentRule.append(latestRule);
-						parentRule = latestRule;
-						latestRuleAdded = true;
-					} else {
-						parentRule = latestRule;
-					}
-				}
-
-				parentRule.raws.semicolon = true;
-				parentRule.append(rule);
-				break;
-			default:
-				console.log('missed one!!', rule.type);
-				break;
-		}
-
-		rule.remove();
+	if (currentLevel === null) {
+		currentLevel = createSelectorLevels(criticalCss, selectorLevels);
 	}
+
+	return currentLevel;
+}
+
+function createSelectorLevels(criticalCss, selectorLevels) {
+	var i = null,
+		currentLevel = null,
+		temp = null;
+
+	numberOfSelectorSearches++;
+
+	currentLevel = criticalCss;
+
+	for (i = 0; i < selectorLevels.length; i++) {
+		temp = selectorLevels[i];
+		currentLevel.append(temp);
+		currentLevel = temp;
+		temp = null;
+	}
+
+	return currentLevel;
+}
+
+function findSelector(criticalCss, selectorLevels) {
+	var result = null,
+		currentLevel = null,
+		temp = null,
+		i = null;
+
+	currentLevel = criticalCss;
+
+	for (i = 0; i < selectorLevels.length; i++) {
+		temp = selectorLevels[i];
+		currentLevel = currentLevel.last;
+
+		if (typeof currentLevel === 'undefined' || areTheSame(temp, currentLevel) === false) {
+			currentLevel = null;
+			break;
+		}
+	}
+
+	result = currentLevel;
+
+	return result;
+}
+
+function areTheSame(a, b) {
+	var tempA = null,
+		tempB = null,
+		result = false;
+
+	numberOfSameChecks++;
+
+	if (a.type === b.type) {
+		tempA = a.clone().removeAll()
+		tempB = b.clone().removeAll();
+
+		if (tempA.toString() === tempB.toString()) {
+			result =  true;
+		}
+	}
+
+	return result;
+}
+
+function getParents(line) {
+	 var parents = [],
+		currentParent = null,
+		temp = null;
+
+	currentParent = line.parent;
+
+	while (typeof currentParent !== 'undefined' && currentParent.type !== 'root') {
+		temp = currentParent.clone();
+		clearLevel(temp);
+		parents.push(temp);
+		temp = null;
+		currentParent = currentParent.parent;
+	}
+
+	parents = parents.reverse();
+
+	return parents;
 }
 
 module.exports = postcss.plugin('postcss-critical-split', CriticalSplit);
